8aaf9c7c40c9ad5f3784277e8d0a5a318ee30530
Maritxiu <68307179+Maritxiu@users.noreply.github.com>
2021-01-04 17:35:56 -0300

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,15 @@
+# import tensorflow as tf
+import numpy as np
+"""
+matrizA = tf.constant([[1,2,-3],[4,0,2]])
+matrizB = tf.constant([[3,1],[2,4],[-1,5]])
+
+
+product = tf.matmul(matrizA, matrizB)
+
+sess = tf.Session()
+result = sess.run(product)
+print(result)
+
+sess.close()
+"""
